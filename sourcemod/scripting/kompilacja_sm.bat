e1d5bb9b71994fb9d8aecde2f059b504b2f7638d
Sebastian K <seba5zer@gmail.com>
2015-02-03 16:55:00 +0100

    Dodanie skryptu bat, do kompilowania

---
+++ sourcemod/scripting/kompilacja_sm.bat
@@ -0,0 +1,74 @@
+@echo off
+
+title Kompilacja plik¢w .sp
+
+cls
+
+rem —cie¾ka do kompilatora
+set compiler=D:\Programy\Source Mod\1.6.3\addons\sourcemod\scripting\spcomp.exe
+
+if not exist "%compiler%" ( echo Kompilator %compiler% nie istnieje & goto WYJSCIE )
+
+rem —cie¾ka do plik¢w .inc
+set include=D:\Programy\Source Mod\1.6.3\addons\sourcemod\scripting\include
+
+rem —cie¾ka do moich plik¢w .inc
+set my_include=E:\Documents\pluginy\sm\include
+
+if not exist "%include%" ( echo Folder %include% nie istnieje & goto WYJSCIE )
+
+rem Miejsce gdzie zostan¥ wrzucone pliki .amxx
+rem Zostaw tak jak jest je˜li pliki .amxx maj¥ by† w tym samym folderze co pliki .sma
+rem UWAGA! Na koäcu ˜cie¾ki zawsze musi by† dodany znak \
+rem . oznacza aktualny folder gdzie zostaˆ uruchomiony skrypt
+set output=..\plugins\
+
+rem Usuwanie starych log¢w oraz tworzenie folderu gdzie maj¥ by† wrzucone pliki .amxx
+if exist logi.log del logi.log
+if "%output%" == ".\" (
+	goto KOMPILACJA
+) else (
+	if not exist "%output%" (
+		( mkdir "%output%" && echo Folder %output% zostaˆ utworzony ) || ( echo Nie udaˆo si© utworzy† folderu %output% & goto WYJSCIE )
+	)
+)
+
+:KOMPILACJA
+
+if %1X == X (
+	goto KOMPILACJA_ALL
+) else (
+	goto KOMPILACJA_1
+)
+
+:KOMPILACJA_ALL
+
+echo Rozpoczynam kompilacj© plik¢w .sp
+
+rem Przeszukanie caˆego aktualnego folderu (sk¥d zostaˆ uruchomiony skrypt) oraz kompilacja
+for %%f in (*.sp) do (
+echo *********** Plik: %%f *********** >> logi.log
+"%compiler%" -i"%include%" -i"include" -i"%my_include%" -o"%output%%%f" "%%f" >> logi.log
+echo ******************************************** >> logi.log
+echo Plik %%f skompilowany )
+
+goto KOMPILACJA_END
+
+:KOMPILACJA_1
+
+set plik=%1
+for /f "useback tokens=*" %%a in ('%plik%') do set plik=%%~a
+
+echo Kompilacja pliku %plik%
+echo ****** Plik: %plik% ****** >> logi.log
+"%compiler%" -i"%include%" -i"include" -i"%my_include%" -o"%plik:~0,-2%smx" "%plik%" >> logi.log
+
+:KOMPILACJA_END
+
+echo Kompilacja zakoäczona.
+echo Komunikaty z kompilacji znajduj¥ si© w pliku logi.log
+
+:WYJSCIE
+
+echo.
+pause
